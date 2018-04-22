fc8c50aa2a8e5d5def0e598338130591eb8afa48
MKMichalski <mateuszmichalskiK@gmail.com>
2018-04-22 14:14:41 +0200

    first commit

---
+++ findNested.js
@@ -0,0 +1,11 @@
+// db = connect("localhost:27017/films")
+
+use films
+
+ db.films.insert({"title" :"Jaja","Year":1967, "director":{"firstName":"Quentin","lastName":"Tara","birthday":"1972-01-22"}})
+
+db.films.find({"director.lastName":"Tara"})
+cursor = db.films.find().pretty()
+while (cursor.hasNext()){
+	printjson(cursor.next());
+}
